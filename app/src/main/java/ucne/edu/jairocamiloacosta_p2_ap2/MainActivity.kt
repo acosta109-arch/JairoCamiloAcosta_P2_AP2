ac9f83a952f68fed63fc70b5df5dacc7b7c2bc4e
Jairo <camiloacostajairo5@gmal.com>
2025-02-26 18:31:41 -0400

    Iniciar examen.

--- app/src/main/java/ucne/edu/jairocamiloacosta_p2_ap2/MainActivity.kt
+++ app/src/main/java/ucne/edu/jairocamiloacosta_p2_ap2/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import ucne.edu.jairocamiloacosta_p2_ap2.presentation.navigation.registro_entity
 import ucne.edu.jairocamiloacosta_p2_ap2.ui.theme.JairoCamiloAcosta_P2_AP2Theme
 
 @AndroidEntryPoint
@@ -21,7 +23,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JairoCamiloAcosta_P2_AP2Theme {
-
+                val navHost = rememberNavController()
+                registro_entity(
+                    navHostController = navHost
+                )
             }
         }
     }
---
+++ app/src/main/java/ucne/edu/jairocamiloacosta_p2_ap2/presentation/navigation/NavigationNavHost.kt
@@ -0,0 +1,7 @@
+package ucne.edu.jairocamiloacosta_p2_ap2.presentation.navigation
+
+import androidx.navigation.NavHostController
+
+fun registro_entity(navHostController: NavHostController){
+
+}
