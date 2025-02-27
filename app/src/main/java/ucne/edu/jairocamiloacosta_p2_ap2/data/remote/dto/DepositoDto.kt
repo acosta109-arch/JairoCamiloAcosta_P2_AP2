a684d7047e50db87312182a5271a89c5cf12c5dd
Jairo <camiloacostajairo5@gmal.com>
2025-02-26 20:13:42 -0400

    Agregando el deposito dto.

---
+++ app/src/main/java/ucne/edu/jairocamiloacosta_p2_ap2/data/remote/dto/DepositoDto.kt
@@ -0,0 +1,9 @@
+package ucne.edu.jairocamiloacosta_p2_ap2.data.remote.dto
+
+class DepositoDto(
+    val idDeposito: Int,
+    val fecha: String,
+    val idCuenta: Int,
+    val concepto: String,
+    val monto: Double
+)
