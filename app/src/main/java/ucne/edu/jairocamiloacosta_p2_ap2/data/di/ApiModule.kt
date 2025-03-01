c0e360e85f56e2e1849579d03f0b8f3f6307fdee
Jairo <camiloacostajairo5@gmal.com>
2025-02-28 22:02:47 -0400

    Agregando la parte local al ApiModule para el first offline.

--- app/src/main/java/ucne/edu/jairocamiloacosta_p2_ap2/data/di/ApiModule.kt
+++ app/src/main/java/ucne/edu/jairocamiloacosta_p2_ap2/data/di/ApiModule.kt
@@ -1,19 +1,36 @@
 package ucne.edu.jairocamiloacosta_p2_ap2.data.di
 
+import android.content.Context
+import androidx.room.Room
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import ucne.edu.jairocamiloacosta_p2_ap2.data.local.database.DepositoDb
 import ucne.edu.jairocamiloacosta_p2_ap2.data.remote.entity.DepositoManagerApi
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            DepositoDb::class.java,
+            "Deposito.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun provideDao(depositoDb: DepositoDb) = depositoDb.depositoDao()
+
     private const val BASE_URL = "https://sagapi-dev.azurewebsites.net/"
 
     @Provides
